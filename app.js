eb6107c2dcd793ff497ebbcf46f8aa6536e5dbac
Pyottness <mark.j.pyott@gmail.com>
2022-01-20 14:32:07 +0100

    added houses info

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+})
+
+app.use('/api/houses', (req, res, next) => {
+  const houses = [
+    {
+      _id: '1',
+      title: 'White House',
+      price: 150000,
+    },
+    {
+      _id: '2',
+      title: 'Orange Brick House',
+      price: 300000,
+    },
+  ];
+  res.status(200).json(houses);
+})
+
+app.use((req, res, next) => {
   console.log('request received!');
   next();
 });
