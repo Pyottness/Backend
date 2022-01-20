88e701ef9921151672102ea22998a847f7cfcdd7
Pyottness <mark.j.pyott@gmail.com>
2022-01-20 14:55:11 +0100

    added a post route

--- app.js
+++ app.js
@@ -2,13 +2,21 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
-})
+});
+
+app.post('/api/houses', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'House created successfully!'
+  });
+});
 
 app.use('/api/houses', (req, res, next) => {
   const houses = [
@@ -24,7 +32,7 @@ app.use('/api/houses', (req, res, next) => {
     },
   ];
   res.status(200).json(houses);
-})
+});
 
 app.use((req, res, next) => {
   console.log('request received!');
